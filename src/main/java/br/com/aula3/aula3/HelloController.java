537250361c6b0586d130d8ae58d37fa1a65113fc
Victor Hugo <vhaporfiro@gmail.com>
2025-03-08 10:27:15 -0300

    Configurando os botões

--- src/main/java/br/com/aula3/aula3/HelloController.java
+++ src/main/java/br/com/aula3/aula3/HelloController.java
@@ -1,8 +1,46 @@
 package br.com.aula3.aula3;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.awt.event.ActionEvent;
 
 public class HelloController {
+
+    @FXML
+    private Button bt_limpar;
+
+    @FXML
+    private Button bt_sair;
+
+    @FXML
+    private Button bt_salvar;
+
+    @FXML
+    private TextField tx_cpf;
+
+    @FXML
+    private TextField tx_endereco;
+
+    @FXML
+    private TextField tx_nome;
+
+    @FXML
+    private TextField tx_telefone;
+
+    @FXML
+    void limpar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sair(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvar(ActionEvent event) {
+
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module br.com.aula3.aula3 {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens br.com.aula3.aula3 to javafx.fxml;
     exports br.com.aula3.aula3;
